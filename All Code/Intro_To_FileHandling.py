7a0fb98e1ab5436b7c26892d06e13c73c6454b0a
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-11-18 17:41:48 +0530

    Create Intro_To_FileHandling.py

---
+++ All Code/Intro_To_FileHandling.py
@@ -0,0 +1,29 @@
+'''
+Introduction to 
+File Handling
+'''
+
+print("------------------------------------------------\n")
+
+# Open a file for reading
+file = open("File.txt", "r")  # "r" stands for read mode
+
+file = open("File.txt", "w")  # "w" stands for Write mode
+
+file = open("File.txt", "a")  # "a" stands for Append mode
+
+file = open("File.txt", "a+")  # "a" stands for Append and read mode
+
+file = open("File.txt", "r+")  # "a" stands for Read and Write mode
+
+file = open("File.txt", "w+")  # "a" stands for Write and Read mode
+
+# Close a file After reading
+
+file.close()
+
+print("\n------------------------------------------------\n")
+
+
+
+print("\n------------------------------------------------")
