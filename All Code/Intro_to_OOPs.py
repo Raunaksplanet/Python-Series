d062a8556462447de833d055d02e3a9c4f43be3a
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-11-18 17:28:05 +0530

    Update Intro_to_OOPs.py

--- All Code/Intro_to_OOPs.py
+++ All Code/Intro_to_OOPs.py
@@ -18,17 +18,20 @@ print("\n------------------------------------------------\n")
 
 print("2.Creating Constructor __init__")
 class Dog:
-    attr1 = ""
     # Instance attribute
     def __init__(self, name):
-        self.attr1 = name
-        print(f"Hello there {self.attr1}")
+        self.name= name
+        print(f"Hello there {self.name}", ", Name by constructor")
+
+    def __del__(self):
+        print(f"Hello there {self.name}", ", Name by destructor")
 
  
 
 str2 = input(str("Enter Name: "))
-Dog(str2)
+objdog = Dog(str2)
 
+del objdog
 
 print("\n------------------------------------------------\n")
 
@@ -66,25 +69,54 @@ else:
 print("\n------------------------------------------------\n")
 
 print("3.Polymorphism in class")
+print("3.1.Method Overriding")
+
+class car:
+    def sound(self):
+        print("Car Sound")
+
+    def start(self):
+        print("Car Start")
+
+class car1(car):
+    def start(self):
+        print("Car Start 1")
+
+class car2(car):
+    def start(self):
+        print("Car Start 2")
 
 
+carObj = car()
+carObj1 = car1()
+carObj2 = car2()
+
+carObj.sound()
+carObj.start()
+
+carObj1.sound()
+carObj1.start()
+
+carObj2.sound()
+carObj2.start()
 
 
+print("\n3.2.Method OverLoading")
+class OverloadedMethods:
+    def add(self, a, b=0, c=0):
+        return a + b + c
 
+# Create an instance of the class
+obj = OverloadedMethods()
 
+# Test different method calls
+result1 = obj.add(1)
+result2 = obj.add(1, 2)
+result3 = obj.add(1, 2, 3)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Print the results
+print("Result 1:", result1)
+print("Result 2:", result2)
+print("Result 3:", result3)
 
 print("\n------------------------------------------------")
