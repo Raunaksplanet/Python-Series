cb7dfcf39a63992774cf6097bf095cb2efde42c0
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-11-19 12:08:55 +0530

    Update Intro_To_FileHandling.py

--- All Code/Intro_To_FileHandling.py
+++ All Code/Intro_To_FileHandling.py
@@ -5,25 +5,49 @@ File Handling
 
 print("------------------------------------------------\n")
 
-# Open a file for reading
+# 1.Reading
 file = open("File.txt", "r")  # "r" stands for read mode
 
-file = open("File.txt", "w")  # "w" stands for Write mode
+# file = open("File.txt", "w")  # "w" stands for Write mode
 
-file = open("File.txt", "a")  # "a" stands for Append mode
+# file = open("File.txt", "a")  # "a" stands for Append mode
 
-file = open("File.txt", "a+")  # "a" stands for Append and read mode
+# file = open("File.txt", "a+")  # "a" stands for Append and read mode
 
-file = open("File.txt", "r+")  # "a" stands for Read and Write mode
+# file = open("File.txt", "r+")  # "a" stands for Read and Write mode
 
-file = open("File.txt", "w+")  # "a" stands for Write and Read mode
+# file = open("File.txt", "w+")  # "a" stands for Write and Read mode
 
-# Close a file After reading
-
-file.close()
+# 2.Closing
+# file.close()
 
 print("\n------------------------------------------------\n")
 
+# 3.Write a file
+# 3.1. Open a file with write mode
+file = open("file.txt", "r+")
+file.write("Hello there, im under the water")
+cc = file.readline()
+print(cc)
+file.close
+
+# 3.1. Open a file with write mode
+
+file = open("file.txt", "a+")
+file.write("\nHello there, im under the water\n")
+cc = file.readline()
+print(cc)
+file.close
 
 
+
+print("\n------------------------------------------------\n")
+
+# content = file.read()
+# content = file.readline()
+# content = file.readlines()
+
+# print(content)
+
+file.close()
 print("\n------------------------------------------------")
